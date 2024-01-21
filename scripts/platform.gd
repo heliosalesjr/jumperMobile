90bde25f246f35aea0168d7991d2138d8b4ce47b
heliosalesjr <heliosalesjr@gmail.com>
2024-01-21 15:43:33 -0300

    player jump

--- scripts/platform.gd
+++ scripts/platform.gd
@@ -2,4 +2,5 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body is Player:
-		print("toloka")
+		if body.velocity.y > 0:
+			body.jump()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,22 +1,28 @@
 extends CharacterBody2D
 class_name Player
+@onready var animator = $AnimationPlayer
 
 var speed = 300.0
 var gravity = 15.0
 var max_fall_velocity = 1000.0
 var viewport_size
+var jump_velocity = -800
 
 func _ready():
 	viewport_size = get_viewport_rect().size
 	
 func _process(delta):
-	pass
+	if velocity.y > 0:
+		if animator.current_animation != "fall":
+			animator.play("fall")
+	if velocity.y < 0:
+		if animator.current_animation != "jump":
+			animator.play("jump")
 	
 func _physics_process(delta):
 	velocity.y += gravity
 	if velocity.y > max_fall_velocity:
 		velocity.y = max_fall_velocity
-	
 	
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
@@ -32,4 +38,5 @@ func _physics_process(delta):
 	if global_position.x < -margin:
 		global_position.x = viewport_size.x + margin
 
-
+func jump():
+	velocity.y = jump_velocity
