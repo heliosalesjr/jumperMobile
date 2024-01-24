163526394d4d910c94756a8756f589a5911dcfb3
heliosalesjr <heliosalesjr@gmail.com>
2024-01-24 11:45:34 -0300

    random platform generation

--- scripts/game.gd
+++ scripts/game.gd
@@ -8,14 +8,38 @@ var camera_scene = preload("res://scenes/game_camera.tscn")
 
 var camera = null
 
+# Level gen variables
+var start_platform_y
+var y_distance_between_platforms = 200
+var level_size = 50
+
+
 func _ready():
 	camera = camera_scene.instantiate()
 	camera.setup_camera($Player)
 	add_child(camera)
 	
-	create_platform(Vector2(100, 300))
-	create_platform(Vector2(100, 500))
-	create_platform(Vector2(100, 800))
+	#the ground
+	var viewport_size = get_viewport_rect().size
+	var platform_width = 136
+	var ground_layer_platform_count = (viewport_size.x / platform_width) + 1
+	var ground_layer_y_offset = 62
+	for i in range(ground_layer_platform_count):
+		var ground_location = Vector2(i * platform_width, viewport_size.y - ground_layer_y_offset)
+		create_platform(ground_location)
+
+	# Generate the rest of the level
+	start_platform_y = viewport_size.y - (y_distance_between_platforms*2 - 100)
+	for i in range(level_size):
+		var max_x_position = viewport_size.x - platform_width
+		var random_x = randf_range(0.0, max_x_position)
+		
+		var location: Vector2
+		location.x = random_x
+		location.y = start_platform_y - (y_distance_between_platforms * i)
+		
+		create_platform(location)
+
 
 
 func _process(delta):
