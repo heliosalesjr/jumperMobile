4069c270ad820f05945872971f5be0cd2e83b10b
heliosalesjr <heliosalesjr@gmail.com>
2024-01-31 21:38:41 -0300

    level gen script + platform destroyer

--- scripts/game_camera.gd
+++ scripts/game_camera.gd
@@ -1,4 +1,6 @@
 extends Camera2D
+@onready var destroyer = $Destroyer
+@onready var destroyer_shape = $Destroyer/CollisionShape2D
 
 var player: Player = null
 
@@ -7,9 +9,19 @@ var viewport_size
 func _ready():
 	viewport_size = get_viewport_rect().size
 	global_position.x = viewport_size.x / 2
+	
 	limit_bottom = viewport_size.y
 	limit_left = 0
 	limit_right = viewport_size.x
+	
+	destroyer.position.y = (viewport_size.y / 2)
+	
+	var rect_shape = RectangleShape2D.new()
+	var rect_shape_size = Vector2(viewport_size.x, 200)
+	rect_shape.set_size(rect_shape_size)
+	destroyer_shape.shape = rect_shape
+	
+	destroyer_shape.shape = rect_shape
 
 func _process(delta):
 	if player:
