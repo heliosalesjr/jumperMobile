29ef7a0683c25d2d8e7e6a2fd58124cbd1a54e8b
heliosalesjr <heliosalesjr@gmail.com>
2024-03-15 20:49:19 -0300

    handling buttons for the start, pause and game over screens

--- scripts/screens.gd
+++ scripts/screens.gd
@@ -3,9 +3,31 @@ extends CanvasLayer
 @onready var console = $Debug/ConsoleLog
 
 func _ready():
-	
 	console.visible = false
+	register_buttons()
 	
+func register_buttons():
+	var buttons = get_tree().get_nodes_in_group("buttons")
+	if buttons.size() > 0:
+		for button in buttons:
+			if button is ScreenButton:
+				button.clicked.connect(_on_button_pressed)
+
+func _on_button_pressed(button):
+	match button.name:
+		"TitlePlay":
+			print("Play button is pressed")
+		"PauseRetry":
+			print("Pause retry")
+		"PauseBack":
+			print("Pause back")
+		"PauseClose":
+			print("Pause close")
+		"GameOverRetry":
+			print("Game over retry")
+		"GameOverBack":
+			print("Game over back")
+
 
 func _process(delta):
 	pass
