d06cbb812c381eb79a0f6d9acbe93eef0c8ae187
heliosalesjr <heliosalesjr@gmail.com>
2024-03-19 22:38:10 -0300

    safe area & screen scale

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,5 +1,27 @@
 extends Control
 
+@onready var topbar = $TopBar
+@onready var topbar_bg = $TopBarBG
+
+func _ready():
+	var os_name = OS.get_name()
+	if os_name == "Android" || os_name == "iOS":
+		var safe_area = DisplayServer.get_display_safe_area()
+		var safe_area_top = safe_area.position.y
+		
+		if os_name == "iOS":
+			var screen_scale = DisplayServer.screen_get_scale()
+			safe_area_top = (safe_area_top / screen_scale)
+			MyUtility.add_log_msg("Screen scale: " + str(screen_scale))
+
+		topbar.position.y += safe_area_top
+		var margin = 10
+		topbar_bg.size.y += safe_area_top + margin
+		
+		MyUtility.add_log_msg("Safe area: " + str(safe_area))
+		MyUtility.add_log_msg("Window size: " + str(DisplayServer.window_get_size()))
+		MyUtility.add_log_msg("safe_area_top: " + str(safe_area_top))
+		MyUtility.add_log_msg("top bar pos: " + str(topbar.position))
 
 func _on_pause_button_pressed():
 	pass # Replace with function body.
